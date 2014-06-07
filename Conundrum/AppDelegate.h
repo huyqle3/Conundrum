960f8cf04bae28c28881a80308df6fae1e696439
huyle333 <huyle333@yahoo.com>
2014-06-07 09:53:12 -0400

    Initial Commit

---
+++ Conundrum/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Conundrum
+//
+//  Created by Le, Huy on 6/7/14.
+//  Copyright (c) 2014 BUILDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Conundrum/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Conundrum
+//
+//  Created by Le, Huy on 6/7/14.
+//  Copyright (c) 2014 BUILDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
